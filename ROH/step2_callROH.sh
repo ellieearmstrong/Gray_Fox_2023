1024086c3a4357e99b3fd5a3977ce0a7e1854150
jaam92 <jazlyn.mooney@gmail.com>
2023-09-24 15:23:16 -0700

    updated for all assemblies and autosize windows

--- ROH/step2_callROH.sh
+++ ROH/step2_callROH.sh
@@ -1,6 +1,11 @@
 
 ###bypass centromeres
-/scratch/users/elliea/jazlyn-ellie/grayfox_2023/software/garlic-master/bin/linux/garlic --tped /scratch/users/elliea/jazlyn-ellie/grayfox_2023/plinkFiles/fox_merge_variants_bisnps_autosome.GM.eh2.AN.qualdp.updatedFID.tped --tfam /scratch/users/elliea/jazlyn-ellie/grayfox_2023/plinkFiles/fox_merge_variants_bisnps_autosome.GM.eh2.AN.qualdp.updatedFID.tfam --centromere fakeCentromeres.txt --error 0.001 --winsize 700 --out /scratch/users/elliea/jazlyn-ellie/grayfox_2023/plinkFiles/fox_merge_variants_bisnps_autosome.GM.eh2.AN.qualdp.updatedFID
+for ref in {Canfam3.1,Canfam4,grayfox,arcticfox}
+do
+
+/scratch/users/elliea/jazlyn-ellie/grayfox_2023/software/garlic/bin/linux/garlic --tped /scratch/users/elliea/jazlyn-ellie/grayfox_2023/plink/"$ref"_filtered.renameChroms.ACgr25_DPgr165lt500.tped --tfam /scratch/users/elliea/jazlyn-ellie/grayfox_2023/plink/"$ref"_filtered.renameChroms.ACgr25_DPgr165lt500.tfam --centromere fakeCentromeres_dog.txt --error 0.001 --winsize 100 --auto-winsize --auto-overlap-frac --out "$ref"
+
+done
 
 
 
