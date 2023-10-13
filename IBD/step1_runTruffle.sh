40f42000b5a03061cd5fd932b5a12d6dd6578048
Jazlyn Mooney <jazlyn.mooney@gmail.com>
2023-10-13 14:19:14 -0700

    Add files via upload

--- IBD/step1_runTruffle.sh
+++ IBD/step1_runTruffle.sh
@@ -1,5 +1,12 @@
  
 
-/scratch/users/elliea/jazlyn-ellie/grayfox_2023/software/truffle/truffle --vcf /scratch/users/elliea/jazlyn-ellie/grayfox_2023/fox_merge_variants_snps_autosome.GM.AN.qualdp.vcf.gz --cpu 4 --segments --missing 1 --maf 0 --nofiltering --out foxes.truffle
 
 
+while read -r p
+do 
+
+/scratch/users/elliea/jazlyn-ellie/grayfox_2023/software/truffle/truffle --vcf /scratch/users/elliea/jazlyn-ellie/grayfox_2023/vcf/"$p" --cpu 4 --segments --missing 1 --maf 0 --nofiltering --out "$p".truffle
+
+echo "$p"
+
+done < vcfIn2.txt
