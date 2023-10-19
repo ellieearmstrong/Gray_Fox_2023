0873e62c6f5a0f9ed37b29d775534e79d7cd1c88
Jazlyn Mooney <jazlynmo@dts.usc.edu>
2023-10-19 11:02:12 -0700

    plotting roh and filtering

--- ROH/ROH_Qual.R
+++ ROH/ROH_Qual.R
@@ -5,29 +5,60 @@ library(mgsub)
 
 #Load files
 setwd("~/Documents/Gray_Fox_2023/ROH/overlapCallable/")
-popDF = read.csv("~/Documents/Gray_Fox_2023/metadata/fox_metadata.csv")
+popDF = read.csv("~/Gray_Fox_2023/metadata/fox_metadata_extended.csv")
 chroms = read.delim("~/Documents/Gray_Fox_2023/metadata/fox_chrom_withLengths.map")
 
-#Load vcftools outputs
-fnames = list.files(pattern = "\\ROH.bed$")
-df = rbindlist(sapply(fnames, fread, simplify = FALSE), use.names = TRUE) #Read all the files into a data frame 
-colnames(df) = c("CHROM", "start", "end", "type" ,"indiv", "numCallableSitesOverlap", "numROHBasesOverlaped", "rohLength", "propCoverage")
-#Remove header lines and convert character columns to numeric
+# #Load vcftools outputs
+# fnames = list.files(pattern = "\\ROH.bed$")
+# df = rbindlist(sapply(fnames, fread, simplify = FALSE), use.names = TRUE) #Read all the files into a data frame 
+# colnames(df) = c("CHROM", "start", "end", "INDV", "numCallableSitesOverlap", "numROHBasesOverlaped", "rohLength", "propCoverage")
 
-filterROH = df %>%
-  select("CHROM", "start","type" , "end", "indiv", "rohLength", "propCoverage") %>%
-  filter(propCoverage >= .60 & propCoverage <= .95 & rohLength >= 10000) %>%
-  mutate(Population = popDF$Locality[match(indiv, popDF$Individual)])
-  
+# #filter on proportion coverage 
+#  filterROHCoverage = df %>%
+#    select("CHROM", "start", "end", "INDV", "rohLength", "propCoverage") %>%
+#    filter(propCoverage >= .60 & rohLength >= 10000) %>% #60 % coverage and at least 10kb
+#    mutate(Ref = gsub('.*\\.', '', INDV),
+#           Ref = gsub("1", "Canfam3.1", Ref),
+#           INDV = gsub("\\..*", "", INDV)) %>%
+#    left_join(allfiles) %>%
+#    left_join(popDF) 
+#  
+# #find shared ROH, mark to drop if more than two individuals have them  
+#  sharedROH = filterROHCoverage %>% 
+#    group_by(CHROM, start, end, Ref) %>% 
+#    count() %>% 
+#    mutate(keep = ifelse(n < 3, "keep", "toss")) 
+#  
+# #Remove shared stuff
+#  filterROH = filterROHCoverage %>%
+#    left_join(sharedROH) %>%
+#    filter(keep == "keep") %>%
+#    mutate(keep = NULL,
+#           Locality_ordered=factor(Locality, 
+#                                   levels = c('San Miguel',
+#                                              'Santa Rosa',
+#                                              'Santa Cruz',
+#                                              'Santa Catalina',
+#                                              'San Nicolas',
+#                                              'San Clemente',
+#                                              'Santa Monica Mountains',
+#                                              'Golden Gate National Rec Area')))
 
-#Alternative way to QC ROH Segments
-##Will remove ROH if the proportion of ROH segment covered by snps is not within a SD of the mean
-#z = data.table(df)
-#z[,ToKeep := abs(df$propCoverage - mean(df$propCoverage)) < sd(df$propCoverage)][ToKeep  == TRUE] #create variable that identifies what to drop or keep 
-#filterROH = subset(z, z$ToKeep == "TRUE")
+#Read in file with filtered ROH 
+ filterROH = read_delim("FinalROHQCd_min10KbminPropCov60per_allIndivs_May2023_garlic.bed", delim = "\t") %>%
+   mutate(keep = NULL,
+          Locality_ordered=factor(Locality, 
+                                  levels = c('San Miguel',
+                                             'Santa Rosa',
+                                             'Santa Cruz',
+                                             'Santa Catalina',
+                                             'San Nicolas',
+                                             'San Clemente',
+                                             'Santa Monica Mountains',
+                                             'Golden Gate National Rec Area')))
 
 
-#write.table(filterROH, file="~/Documents/Gray_Fox_2023/ROH/FinalROHQCd_min10KbminPropCov60per_allIndivs_May2023_garlic.bed", quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+#write.table(filterROH, file="~/Gray_Fox_2023/ROH/FinalROHQCd_PropCovgrThan60per_allIndivs_Oct2023_garlic.bed", quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
 
 #Plot the distribution of coverage of ROHs post-filtering
 postFilter = ggplot() + 
@@ -41,35 +72,67 @@ postFilter = ggplot() +
         axis.title=element_text(size=24))
 #print(postFilter)
 
-#remove anything less than 1Mb and approximate FROH
-genomeLength = sum(chroms$length)
 
-IndividualLevel = filterROH %>%
-  filter(type == "C") %>%
-  group_by(indiv) %>% 
+#Compute FROH with the total ungapped length
+FROH = filterROH %>%
+  filter(ROHCategory == "C") %>%
+  group_by(Ref, INDV) %>% 
   summarise(TotalROH = sum(rohLength)) %>%
-  mutate(TotalROHMb = TotalROH/10^6, 
-         FROH = TotalROH/genomeLength,
-         Population = popDF$Locality[match(indiv, popDF$Individual)])
+  mutate(FROH = case_when(
+           Ref == "Canfam4" ~ TotalROH/2482000080,
+           Ref == "Canfam3.1" ~ TotalROH/2392715236,
+           Ref == "arcticfox" ~ TotalROH/2345550353,
+           Ref == "grayfox" ~ TotalROH/2658766243)) %>%
+  left_join(popDF) %>%
+  mutate(Locality_ordered=factor(Locality, 
+                                 levels = c('San Miguel',
+                                            'Santa Rosa',
+                                            'Santa Cruz',
+                                            'Santa Catalina',
+                                            'San Nicolas',
+                                            'San Clemente',
+                                            'Santa Monica Mountains',
+                                            'Golden Gate National Rec Area')))
 
 
 
-#Plot FROH with ROH greater than 1Mb
-#prep data for plotting
+#Plotting
+#color palette
 cbPalette <- c("Golden Gate National Rec Area" = "#FBD05C", "Santa Monica Mountains" = "#B2A6CC", 
                "Santa Catalina" ="#8FFFF5", "Santa Cruz" = "#4C8C42", "San Clemente" = "#3854A6", 
                "San Nicolas"= "#F25C05", "Santa Rosa" = "#B20650", "San Miguel" = "#6F4468", "Cinereoargenteus" = "gray20")
 
-FROHplot = ggplot(IndividualLevel, aes(x=Population, y=FROH, colour=Population)) + 
-  scale_colour_manual(name = "Population", values = cbPalette) + 
-  geom_boxplot(size=1) + 
-  geom_point(show.legend=F) + 
+FROHplot = ggplot(FROH, aes(x=Locality_ordered, y=FROH, color = Locality)) + 
+  facet_wrap(Ref ~ .) +
+  geom_point(size = 8) + 
   theme_bw() + 
-  labs(x = "Population", y=expression(F[ROH])) + 
-  theme(axis.text.x = element_text(size = 20), 
+  labs(x = "Locality", y=expression(F[ROH])) +
+  scale_color_manual(name = "Locality", values = cbPalette) +
+  theme(axis.text.x = element_blank(), 
         axis.text.y = element_text(size = 20), 
         plot.title=element_text(size=24, face = "bold", hjust=0.5), 
         axis.title=element_text(size=20),
+        strip.text = element_text(size=20),
         legend.title=element_text(size=20), 
-        legend.text=element_text(size=18))
+        legend.text=element_text(size=18),
+        legend.position = "right")
+print(FROHplot)
 
+
+plotROH = ggplot(filterROH, aes(x=Locality_ordered, y=rohLength/10^6, colour = Locality)) + 
+  facet_grid(Ref ~ ROHCategory, scales = "free_x") +
+  geom_boxplot() + 
+  geom_point(color = "black", show.legend = F) +
+  coord_flip() +
+  theme_bw() + 
+  labs(y = "Locality", x="ROH Length (Mb)") +
+  scale_colour_manual(name = "Locality", values = cbPalette) +
+  theme(axis.text.y = element_text(size = 20), 
+        axis.text.x = element_text(size = 20), 
+        plot.title=element_text(size=24, face = "bold", hjust=0.5), 
+        axis.title=element_text(size=20),
+        strip.text = element_text(size=20),
+        legend.title=element_text(size=20), 
+        legend.text=element_text(size=18),
+        legend.position = "none")
+print(plotROH)
