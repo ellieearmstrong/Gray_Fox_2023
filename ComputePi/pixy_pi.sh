b91ea46bd0a9baa5fe64cd65ceb262abc1f1d4b9
Jazlyn Mooney <jazlyn.mooney@gmail.com>
2024-02-20 22:42:32 -0800

    Create pixy_pi.sh
    script to compute pi with pixy instead of vcftools because it is faster

---
+++ ComputePi/pixy_pi.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#SBATCH --job-name=piAF
+#SBATCH --output=/scratch1/jazlynmo/grayfox/logfiles/pi_%a_af.out  #you will need to modify the path
+#SBATCH --error=/scratch1/jazlynmo/grayfox/logfiles/pi_%a_af.err  #you will need to modify the path
+#SBATCH --time=01:00:00 #ten hour run time
+#SBATCH -p qcb #the main partition
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32 #one cpu per task
+#SBATCH --array=1-38
+#SBATCH --mem-per-cpu=1000MB #this is equivalent to 1G
+#SBATCH --mail-type=END,FAIL # notifications for job done & fail
+#SBATCH --mail-user=jazlynmo@usc.edu
+
+#load everything up
+module purge
+eval "$(conda shell.bash hook)"
+module load gcc/8.3.0 python/3.6.8
+conda activate pixy-env
+
+#set variables
+CHROM=$SLURM_ARRAY_TASK_ID
+
+
+####Mainland
+
+while read -r infile
+do
+
+##pixy will not allow the path in the output_prefix you would have to specificy that with a folder
+##already know this file include invariant sites so I don't need the check
+##compute pi in 100kb windows for each chromosome
+
+pixy --stats pi --vcf /scratch1/jazlynmo/grayfox/vcf/Mainland/"$infile".gvcf.gz --populations /scratch1/jazlynmo/grayfox/metaData/pops_pixy_n12.txt --window_size 100000 --chromosomes "chr"$CHROM --bypass_invariant_check 'yes' --n_cores 32 --output_folder /scratch1/jazlynmo/grayfox/Analyses/pi/output_pi/ --output_prefix $infile"_"$CHROM
+
+echo $infile
+
+done < gvcfIn_Mainland.txt
+
+
+####Channel Island
+
+while read -r infile
+do
+
+##pixy will not allow the path in the output_prefix you would have to specificy that with a folder
+##already know this file include invariant sites so I don't need the check
+##compute weir and cockerham fst in 100kb windows for each chromosome
+
+pixy --stats pi --vcf /scratch1/jazlynmo/grayfox/vcf/ChannelIsland/"$infile".gvcf.gz --populations /scratch1/jazlynmo/grayfox/metaData/pops_pixy_ChannelIsland.txt --window_size 100000 --chromosomes "chr"$CHROM --bypass_invariant_check 'yes' --n_cores 32 --output_folder /scratch1/jazlynmo/grayfox/Analyses/pi/output_pi_ChannelIsland/ --output_prefix $infile"_"$CHROM
+
+echo $infile
+
+
+done < gvcfIn_ChannelIsland.txt
+
+sleep 180
