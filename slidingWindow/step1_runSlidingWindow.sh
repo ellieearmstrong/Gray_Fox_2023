4e7c042386e5baa7282287f08d08df7dcce14719
Jazlyn Mooney <jazlyn.mooney@gmail.com>
2024-03-30 20:59:26 -0700

    Update step1_runSlidingWindow.sh
    running on USC cluster

--- slidingWindow/step1_runSlidingWindow.sh
+++ slidingWindow/step1_runSlidingWindow.sh
@@ -1,32 +1,43 @@
-#SBATCH --job-name=slidingwin
-#SBATCH --output=/scratch/users/elliea/jazlyn-ellie/grayfox_2023/logfiles/slidingwin.out  #you will need to modify the path
-#SBATCH --error=/scratch/users/elliea/jazlyn-ellie/grayfox_2023/logfiles/slidingwin.err  #you will need to modify the path
-#SBATCH --time=10:00:00 #run time
-#SBATCH -p normal #the main partition
+#!/bin/sh
+#SBATCH --job-name=slideHet
+#SBATCH --output=/scratch1/jazlynmo/grayfox/logfiles/slideHet.out  #you will need to modify the path
+#SBATCH --error=/scratch1/jazlynmo/grayfox/logfiles/slideHet.err  #you will need to modify the path
+#SBATCH --time=10:00:00 #one hour run time
+#SBATCH -p qcb #the main partition
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1 #one cpu per task
-#SBATCH --mem-per-cpu=4000MB #this is equivalent to 4G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=5000MB #this is equivalent to 5G
 #SBATCH --mail-type=END,FAIL # notifications for job done & fail
-#SBATCH --mail-user=jazlyn.mooney@gmail.com
+#SBATCH --mail-user=jazlynmo@usc.edu
 
 #load python and pysam
-ml biology python/2.7.13
-ml biology py-pysam/0.14.1_py27
+module load gcc/11.3.0 openblas/0.3.20 python/3.9.12 py-pysam/0.18.0
 
-inputDir='/scratch/users/elliea/jazlyn-ellie/grayfox_2023/vcf/splitChroms'
-outputDir='/scratch/users/elliea/jazlyn-ellie/grayfox_2023/Analyses/slidingWindow/'
 
-#finish can fam genomes
+inputDir='/scratch1/jazlynmo/grayfox/vcf/Mainland/splitChroms/'
+outputDir='/scratch1/jazlynmo/grayfox/Analyses/slidingWindow/output_Mainland/'
 
+
+#run for gray fox
 SECONDS=0
-
-for ref in {Canfam3.1,Canfam4}
+for CHROM in {31..32}
 do
-        for CHROM in {1..38}
+        #command to run script
+        master_VCF=${inputDir}/'grayfox_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500_chr'${CHROM}'.vcf.gz'
+        python3 SlidingWindowProvideChromSize.py --vcf ${master_VCF} --window_size 100000 --step_size 10000 --chromNum chr${CHROM} --chromLengths grayfox_chromosome_lengths.txt --outpath ${outputDir}
+
+done
+echo $SECONDS
+
+#run can fam and graywolf
+SECONDS=0
+for ref in {Canfam3.1,Canfam4,graywolf}
+do
+        for CHROM in {37..38}
         do
 	#command to run script
-	master_VCF=${inputDir}/'chr'${CHROM}'_'${ref}'_filtered.renameChroms.ACgr25_DPgr165lt500.vcf.gz'
-	python SlidingWindowHet_Unphased.jar.ab.jam.${ref}.py --vcf ${master_VCF} --window_size 100000 --step_size 10000 --chromNum chr${CHROM} --outpath ${outputDir}
+	master_VCF=${inputDir}/${ref}'_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500_chr'${CHROM}'.vcf.gz'
+	python3 SlidingWindowProvideChromSize.py --vcf ${master_VCF} --window_size 100000 --step_size 10000 --chromNum chr${CHROM} --chromLengths ${ref}'_chromosome_lengths.txt' --outpath ${outputDir}
 
 	done
 
@@ -36,26 +47,13 @@ done
 
 echo $SECONDS
 
-#run for gray fox
-SECONDS=0
-for CHROM in {1..32}
-do
-	#command to run script
-        master_VCF=${inputDir}/'chr'${CHROM}'_grayfox_filtered.renameChroms.ACgr25_DPgr165lt500.vcf.gz'
-        python SlidingWindowHet_Unphased.jar.ab.jam.grayfox.py --vcf ${master_VCF} --window_size 100000 --step_size 10000 --chromNum chr${CHROM} --outpath ${outputDir}
-
-done
-echo $SECONDS
-
-
 #run for arctic fox
 SECONDS=0
-for CHROM in {1..24}
+for CHROM in {23..24}
 do
         #command to run script
-        master_VCF=${inputDir}/'chr'${CHROM}'_arcticfox_filtered.renameChroms.ACgr25_DPgr165lt500.vcf.gz'
-        python SlidingWindowHet_Unphased.jar.ab.jam.arcticfox.py --vcf ${master_VCF} --window_size 100000 --step_size 10000 --chromNum chr${CHROM} --outpath ${outputDir}
+        master_VCF=${inputDir}/'arcticfox_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500_chr'${CHROM}'.vcf.gz'
+        python3 SlidingWindowProvideChromSize.py --vcf ${master_VCF} --window_size 100000 --step_size 10000 --chromNum chr${CHROM} --chromLengths arcticfox_chromosome_lengths.txt --outpath ${outputDir}
 
 done
 echo $SECONDS
-
