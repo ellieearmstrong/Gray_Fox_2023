f7d9380c6b479820321471040796a1b0de92fa2d
Jazlyn Mooney <jazlyn.mooney@gmail.com>
2024-08-27 11:05:05 -0700

    Update generateVCFs_Mainland_gf_drop295.sh
    condensed and new mappability file that combined repeats

--- filtering/generateVCFs_Mainland_gf_drop295.sh
+++ filtering/generateVCFs_Mainland_gf_drop295.sh
@@ -11,46 +11,33 @@
 #SBATCH --mail-user=jazlynmo@usc.edu
 
 #load modules that you need
-
 module load gcc/11.3.0  openblas/0.3.21 bcftools/1.14
 module load htslib/1.17
 
-####Each filtering step takes about 13 hours
 #Filter out repeat regions and low mappability regions and down get all sites file is biallelic sites that pass filters but may or may not be snps
-#gray fox
-#SECONDS=0
-#bcftools view -M 2 /scratch1/jazlynmo/grayfox/raw_data/gray_fox/batch3/sacks_hc_gray/batch3-mainland-grayfox.merged.allsites.vcf.gz | bcftools view -T ^/scratch1/jazlynmo/grayfox/genmap_repmask/grayfox_mappability_genmap.1.0.bed -T ^/scratch1/jazlynmo/grayfox/genmap_repmask/ArcticFox_repeatmask.bed | bcftools annotate --rename-chrs /scratch1/jazlynmo/grayfox/metaData/grayfox_renameChroms_number.txt -Oz -o /scratch1/jazlynmo/grayfox/vcf/Mainland/grayfox_filtered.renameChroms.Mainland.gvcf.gz
-#echo $SECONDS
 
-#SECONDS=0
-#tabix -p vcf /scratch1/jazlynmo/grayfox/vcf/Mainland/grayfox_filtered.renameChroms.Mainland.gvcf.gz
-#echo $SECONDS
-
-###Repeat filtering with gray fox
-#Remove the problematic individual and recode the info column
-#delete SRR24465295 from individuals text then retry other filtering
-#SECONDS=0
-#bcftools view /scratch1/jazlynmo/grayfox/vcf/Mainland/grayfox_filtered.renameChroms.Mainland.gvcf.gz -s ^SRR24465295 |  bcftools plugin fill-tags -Oz -o /scratch1/jazlynmo/grayfox/vcf/Mainland/grayfox_filtered.renameChroms.Mainland.drop295.gvcf.gz
-#echo $SECONDS
-
-#SECONDS=0
-#tabix -p vcf /scratch1/jazlynmo/grayfox/vcf/Mainland/grayfox_filtered.renameChroms.Mainland.drop295.gvcf.gz
-#echo $SECONDS
-
-##Filtering based on depth and allele count
 SECONDS=0
-bcftools filter -i 'AC > 59 && INFO/DP > 204 && INFO/DP < 501' /scratch1/jazlynmo/grayfox/vcf/Mainland/unfiltered/grayfox_filtered.renameChroms.Mainland.drop295.gvcf.gz | bcftools view --max-alleles 2 --exclude-types indels -Oz -o /scratch1/jazlynmo/grayfox/vcf/Mainland/grayfox_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500.gvcf.gz
+bcftools view --max-alleles 2 --exclude-types indels /scratch/users/elliea/jazlyn-ellie/mainlandgf_gfref_merge_allsites_Jan24.g.vcf.gz | bcftools view -T ^/scratch/users/elliea/jazlyn-ellie/grayfox_mappability_genmap.1.0.bed | bcftools annotate --rename-chrs /scratch/users/elliea/jazlyn-ellie/grayfox_renameChroms_number.txt -Oz -o  /scratch/users/elliea/jazlyn-ellie/grayfox_filtered.renameChroms.Mainland.gvcf.gz
 echo $SECONDS
 
 SECONDS=0
-tabix -p vcf /scratch1/jazlynmo/grayfox/vcf/Mainland/grayfox_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500.gvcf.gz
+tabix -p vcf  /scratch/users/elliea/jazlyn-ellie/grayfox_filtered.renameChroms.Mainland.gvcf.gz
+echo $SECONDS
+
+##Filtering based on depth and allele count
+SECONDS=0
+bcftools filter -i 'AC > 59 && INFO/DP > 204 && INFO/DP < 501'  /scratch/users/elliea/jazlyn-ellie/grayfox_filtered.renameChroms.Mainland.gvcf.gz -Oz -o  /scratch/users/elliea/jazlyn-ellie/grayfox_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500.gvcf.gz
+echo $SECONDS
+
+SECONDS=0
+tabix -p vcf  /scratch/users/elliea/jazlyn-ellie/grayfox_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500.gvcf.gz
 echo $SECONDS
 
 #####Turn the files into snp only vcfs
 SECONDS=0
-bcftools view -m2 -M2 -v snps /scratch1/jazlynmo/grayfox/vcf/Mainland/grayfox_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500.gvcf.gz -Oz -o /scratch1/jazlynmo/grayfox/vcf/Mainland/grayfox_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500.vcf.gz
+bcftools view -m2 -M2 -v snps  /scratch/users/elliea/jazlyn-ellie/grayfox_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500.gvcf.gz -Oz -o  /scratch/users/elliea/jazlyn-ellie/grayfox_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500.vcf.gz
 echo $SECONDS
 
 SECONDS=0
-tabix -p vcf /scratch1/jazlynmo/grayfox/vcf/Mainland/grayfox_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500.vcf.gz
+tabix -p vcf  /scratch/users/elliea/jazlyn-ellie/grayfox_filtered.renameChroms.Mainland.drop295.ACgr59_DPgr205lt500.vcf.gz
 echo $SECONDS
