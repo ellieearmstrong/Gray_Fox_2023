f654b982314d5ce00d016f55912777aedb29caec
Jazlyn Mooney <jazlyn.mooney@gmail.com>
2024-08-28 09:07:19 -0700

    Update generateVCFs_ChannelIsland_canfam4.sh

--- filtering/generateVCFs_ChannelIsland_canfam4.sh
+++ filtering/generateVCFs_ChannelIsland_canfam4.sh
@@ -31,7 +31,7 @@ module load htslib/1.17
 
 #Canfam4
 SECONDS=0
-bcftools filter -e 'AC > 23 && INFO/DP > 164 && INFO/DP < 501' /scratch1/jazlynmo/grayfox/vcf/ChannelIsland/unfiltered/Canfam4_filtered.renameChroms.gvcf.gz |  bcftools reheader -s /scratch1/jazlynmo/grayfox/metaData/rename_Canfam4 | bcftools view --max-alleles 2 --exclude-types indels -Oz -o /scratch1/jazlynmo/grayfox/vcf/ChannelIsland/Canfam4_filtered.renameChroms.ACgr25_DPgr165lt500.gvcf.gz
+bcftools filter -e 'AN > 23 && INFO/DP > 164 && INFO/DP < 501' /scratch1/jazlynmo/grayfox/vcf/ChannelIsland/unfiltered/Canfam4_filtered.renameChroms.gvcf.gz |  bcftools reheader -s /scratch1/jazlynmo/grayfox/metaData/rename_Canfam4 | bcftools view --max-alleles 2 --exclude-types indels -Oz -o /scratch1/jazlynmo/grayfox/vcf/ChannelIsland/Canfam4_filtered.renameChroms.ACgr25_DPgr165lt500.gvcf.gz
 echo $SECONDS
 
 SECONDS=0
